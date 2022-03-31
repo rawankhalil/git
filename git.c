9ac3617c5a95150a38065839f3b84ac166a3a608
rawan elshahawi <rawanelshahawikhalil@gmail.com>
2022-03-31 23:09:14 +0200

    Added day1 &  day2

---
+++ git.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main () 
+{
+	
+	
+/*lab1*/ 
+
+/*
+printf(" Iam Rawan Elshahawi\n" 
+" My Birth date is 2 oct 2001\n" 
+" I Study in faculty of Engineering\n"
+" Mansoura University ");
+	
+*/
